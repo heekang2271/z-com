d9c740e29c2ce40ba3ff872467b99b592da43cb5
heekang2271 <hk.shin2271@gmail.com>
2025-03-24 11:29:21 +0900

    ui 구현

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,3 +1,18 @@
+import Room from './_components/Room';
+import style from './message.module.css';
+
 export default function Messages() {
-  return <main>쪽지 페이지</main>;
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+    </main>
+  );
 }
--- src/app/(beforeLogin)/@modal/(.)i/flow/signup/page.tsx
+++ src/app/(beforeLogin)/@modal/(.)i/flow/signup/page.tsx
@@ -1,5 +1,10 @@
 import SignupModal from '@/app/(beforeLogin)/_components/SignupModal';
 
 export default function Page() {
-  return <SignupModal />;
+  return (
+    <>
+      난 가로채기지롱 ㅋㅋ
+      <SignupModal />
+    </>
+  );
 }
